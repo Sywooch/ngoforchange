019662cd75226c40f0529bb0688b9de90b01d473
sokecze <sokecze@yahoo.com>
2015-07-04 15:20:43 +0300

    Add contacts form fix pt. 2

--- web/js/persons/formcontacts.js
+++ web/js/persons/formcontacts.js
@@ -1,12 +1,82 @@
 $(document).ready(function() {
     
+    window.getTypeIcon = function(type) {
+        var icon =  contactTypeIcons[type] || '';
+        return '<img src="' + icon + '" alt="' + type + '" title="' + type + '" class="contact_type_icon" />';
+    }
+
+    window.getPerson = function(person) {
+        if(!person)
+            return 'No person is assigned';
+        return person.first_name + ' ' + person.last_name;
+    }
+
     $('#conatct-form-list').kendoGrid({
-    	dataSource: [
-	        { productName: "Tea", category: "Beverages" },
-	        { productName: "Coffee", category: "Beverages" },
-	        { productName: "Ham", category: "Food" },
-	        { productName: "Bread", category: "Food" }
-    	],
+    	        dataSource: {
+            transport: {
+                create: {
+                    url: links.contact_create,
+                    type: "POST",
+                    dataType: "json"
+                },
+                read: {
+                    url: links.contact_read,
+                    type: "GET",
+                    dataType: "json"
+                },
+                update: {
+                    url: links.contact_update,
+                    type: "POST",
+                    dataType: "json"
+                },
+                destroy: {
+                    url: links.contact_destroy,
+                    type: "POST",
+                    dataType: "json"
+                },
+                parameterMap: function(options, operation) {
+                    if (operation !== "read" && options.models) {
+                        return kendo.stringify(options.models);
+                    }
+                }
+            },
+            schema: {
+                model: {
+                    id: "id",
+                    fields: {
+                        id: {
+                            type: "string"
+                        },
+                        contact_type: {
+                            type: "string"
+                        },
+                        contact_type_icon: {
+                            type: "string"
+                        },
+                        contact_data: {
+                            type: "string"
+                        },
+                        contact_data_2: {
+                            type: "string"                        
+                        }
+                    }
+                },
+                data: function(response) {
+                    return response.body;
+                },
+                total: function(response) {
+                    return response.body.length; // total is returned in the "total" field of the response
+                },
+                errors: function(response) {
+                    if (response.error == '1')
+                        return response.body;
+                }
+            },
+            error: function(e) {
+                console.log(e.errors); // displays "Invalid query"
+            },
+            pageSize: 20
+        },
         editable: {
             mode: "popup",
             createAt: "bottom",
@@ -37,6 +107,25 @@ $(document).ready(function() {
             title: "&nbsp;",
             width: 319
         }, {
+            field: "id",
+            title: labels.grid_column_id,
+            width: 50,
+        }, {
+            field: "contact_type_icon",
+            title: "&nbsp;",
+            filterable: false,
+            width: 32,
+            template: '#=window.getTypeIcon(contact_type)#'
+        }, {
+            field: "contact_type",
+            title: labels.grid_column_data,
+            width: 150,
+        }, {
+            field: "contact_data",
+            title: labels.grid_column_data,
+            width: 150
+        }
+        /*{
             
             title: labels.grid_column_id,
             width: 50
@@ -48,7 +137,7 @@ $(document).ready(function() {
             field: "category",
             title: labels.grid_column_lname,
             width: 150
-        }, /*{
+        }, *//*{
             field: "types",
             title: labels.grid_column_types,
             width: 150,
